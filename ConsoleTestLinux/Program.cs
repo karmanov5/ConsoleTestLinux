0c84f29bced69d050905d7e42f3cd97c2ecca0db
Karmanov Vladislav <karmanovvladislav@yandex.ru>
2022-05-29 17:08:11 +0300

    Add project files.

---
+++ ConsoleTestLinux/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ConsoleTestLinux;
+
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var user = new User("Vlad", "5555");
+        Console.WriteLine(user.Name);
+        new Thread(() =>
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Hello world!");
+                Thread.Sleep(1000);
+            }
+        }).Start();
+        Console.ReadLine();
+    }
+}
+
